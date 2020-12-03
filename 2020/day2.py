bcf77e332476df54e96d756b0a9494eb1bd91eab
Matthias1590 <matthiasx95@gmail.com>
2020-12-03 20:04:36 +0100

    Added solutions for day 1-3

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,7 +1,8 @@
-# PART 1
-e = 0
 with open("input.txt", "r") as inputFile:
     puzzleInput = inputFile.readlines()
+
+# PART 1
+e = 0
 for line in puzzleInput:
     a, b, c, d = line.split("-")[0], line.split("-")[0].split(" ")[0], line.split(" ")[1].split(":")[0], line.split(" ")[2]
     if d.count(c) >= int(a) <= int(b):
@@ -10,8 +11,6 @@ print(e)
 
 # PART 2
 e = 0
-with open("input.txt", "r") as inputFile:
-    puzzleInput = inputFile.readlines()
 for line in puzzleInput:
     a, b, c, d = line.split("-")[0], line.split("-")[1].split(" ")[0], line.split(" ")[1].split(":")[0], line.split(" ")[2]
     if c == d[int(a)-1] != d[int(b)-1] or c == d[int(b)-1] != d[int(a)-1]:
--- 2020/day3.py
+++ 2020/day3.py
@@ -1,6 +1,7 @@
-# PART 1 AND 2
 with open("input.txt", "r") as inputFile:
     puzzleInput1 = inputFile.readlines(); puzzleInput2 = []
+
+# PART 1 AND 2
 for line in puzzleInput1:
     puzzleInput2.append(line.strip() * 3 * len(puzzleInput1))
 puzzleInput = puzzleInput2; yPos = 0; xPos = 0; trees = 0
