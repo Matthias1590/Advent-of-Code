12d470f5a20967591b69657065c614c05aa9f2a8
Matthias1590 <matthiasx95@gmail.com>
2020-12-03 19:57:48 +0100

    Adding solutions for day 1-3

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,3 +1,14 @@
+# PART 1
+e = 0
+with open("input.txt", "r") as inputFile:
+    puzzleInput = inputFile.readlines()
+for line in puzzleInput:
+    a, b, c, d = line.split("-")[0], line.split("-")[0].split(" ")[0], line.split(" ")[1].split(":")[0], line.split(" ")[2]
+    if d.count(c) >= int(a) <= int(b):
+        e += 1
+print(e)
+
+# PART 2
 e = 0
 with open("input.txt", "r") as inputFile:
     puzzleInput = inputFile.readlines()
